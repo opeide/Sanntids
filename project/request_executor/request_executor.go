02f90a7c2ccdadeecb6169f00e9b84899f944edc
opeide <fredrikopeide@gmail.com>
2017-03-02 22:33:52 +0100

    continued writing

--- project/request_executor/request_executor.go
+++ project/request_executor/request_executor.go
@@ -3,10 +3,12 @@ package request_executor
 import (
 	"../hardware_interface/elev"
 	"../request"
+	"time"
 
 )
 
 var last_motor_direction int
+var current_elevator_floor int
 var last_visited_floor int
 
 type elevator_state_t int
@@ -23,6 +25,9 @@ var requests_upward [elev.N_FLOORS]request.Request
 var requests_downward [elev.N_FLOORS]request.Request
 
 func Execute_requests(requests_to_execute_chan <-chan request.Request, executed_requests_chan chan<- request.Request, floor_changes_chan <-chan int) {
+	
+	elevator_initialize_position()
+	
 	for {
 		select {
 		case request_to_execute := <-requests_to_execute_chan:
@@ -43,22 +48,61 @@ func Execute_requests(requests_to_execute_chan <-chan request.Request, executed_
 						requests_upward[request_to_execute.Floor] = request_to_execute
 					}	
 			}
+
+			elevator_complete_request_at_current_floor()
+
+			elevator_move_in_correct_direction()
 		}
 
-		case elevator_floor := <-floor_changes_chan:		//TODO: Implement floor_changes_chan
-			if elevator_floor == -1{
-				break
-			}
-			last_visited_floor = elevator_floor
+		case current_elevator_floor := <-floor_changes_chan:	
+			if current_elevator_floor == -1 {break}
 
-			if len(requests_downward[elevator_floor]) != 0 || len(requests_upward[request_to_execute.Floor]) != 0{
-				elev.Elev_set_motor_direction(elev.MOTOR_DIRECTION_STOP)
-				//send requests completed
-				//delete requests
-			}
-
-		default:
-			//figure out which direction to move
+			last_elevator_floor = current_elevator_floor
+			elevator_complete_request_at_current_floor()
+			elevator_move_in_correct_direction()
 						
 	}
+}
+
+func elevator_initialize_position(){
+
+}
+
+func elevator_complete_request_at_current_floor(){
+	if current_elevator_floor == -1 {break}
+	if len(requests_downward[elevator_floor]) != 0{
+		elev.Elev_set_motor_direction(elev.MOTOR_DIRECTION_STOP)	//TODO: Turn off lights and open doors
+		executed_requests_chan <- requests_downward[elevator_floor]
+		requests_downward[elevator_floor] = nil 
+	}
+
+	if len(requests_upward[elevator_floor]) != 0{
+		elev.Elev_set_motor_direction(elev.MOTOR_DIRECTION_STOP)	//TODO: Turn off lights and open doors
+		executed_requests_chan <- requests_upward[elevator_floor]
+		requests_upward[elevator_floor] = nil
+
+}	
+
+func elevator_move_in_correct_direction(){
+	if elevator_floor == -1 {break}
+
+	if last_motor_direction == elev.MOTOR_DIRECTION_DOWN{
+		has_requests_below := 0
+		for (i = 0; i < last_elevator_floor; i++){
+			has_requests_below += len(requests_downward[i])
+		}
+		if has_requests_below{
+			elev.Elev_set_motor_direction(elev.MOTOR_DIRECTION_DOWN)
+		}
+	}
+	
+	has_requests_above := 0
+	for (i = last_elevator_floor+1; i < elev.N_FLOORS; i++){
+		has_requests_above, += len(requests_downward[i])
+	}
+	if has_requests_above{
+		elev.Elev_set_motor_direction(elev.MOTOR_DIRECTION_UP)
+	}
+
+	
 }
