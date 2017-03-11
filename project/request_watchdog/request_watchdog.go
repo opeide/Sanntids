5786e78bb751e58474e05e715564c389cbbf44d3
Fredrik <fredrikopeide@gmail.com>
2017-03-11 12:45:53 +0100

    made files for watchdog

---
+++ project/request_watchdog/request_watchdog.go
@@ -0,0 +1,18 @@
+package request_watchdog
+
+import(
+	"time"
+	"fmt"
+	"message_structs"
+)
+
+
+func 
+
+func request_timer_start(request message_structs.Request){
+
+}
+
+func request_timer_stop(request message_structs.Request){
+
+}
