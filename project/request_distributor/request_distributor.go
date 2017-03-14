9afe409f3aead84131ba5744987d69b149db86c5
tobulf <fredrikopeide@gmail.com>
2017-03-14 04:56:58 +0100

    untested for multiple elevators: added request holder

--- project/request_distributor/request_distributor.go
+++ project/request_distributor/request_distributor.go
@@ -6,16 +6,13 @@ import (
 	"../network/peers"
 	"../request_executor"
 	"../request_watchdog"
+	"./request_holder"
 	"fmt"
 	"os"
-	"sort"
 	"time"
 )
 
 var all_elevator_states = make(map[string]message_structs.Elevator_state)
-var all_upward_requests = make(map[string][]message_structs.Request)
-var all_downward_requests = make(map[string][]message_structs.Request)
-var all_command_requests = make(map[string][]message_structs.Request)
 
 var zero_request message_structs.Request = message_structs.Request{}
 
@@ -30,7 +27,6 @@ const (
 	network_transmit_wait_time   = 5 // Milliseconds
 )
 
-// [solved] Remember to do caborders when we get back after losing power!!!!!!!!!!!!!!! solved: get them from network
 // If we lose network, will we be filling up the network channel and eventually block???????
 
 func Distribute_requests(
@@ -53,13 +49,11 @@ func Distribute_requests(
 	requests_to_execute_chan = requests_to_execute_chan_parameter
 	set_lamp_chan = set_lamp_chan_parameter
 
+	request_holder.Attempt_init_id(local_id)
+
 	select {
 	case local_elevator_state := <-local_elevator_state_changes_chan:
-		update_own_state_varialbe_and_send_to_network(local_elevator_state)
-
-		all_upward_requests[local_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_downward_requests[local_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_command_requests[local_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
+		update_own_state_variable_and_send_to_network(local_elevator_state)
 	}
 
 	for {
@@ -67,25 +61,16 @@ func Distribute_requests(
 		case peer_update := <-peer_update_chan:
 			if peer_update.New != "" {
 				fmt.Println("Distributor: New Peer: ", peer_update.New)
-				if _, ok := all_command_requests[peer_update.New]; !ok {
-					fmt.Println("creating new request list for brand new peer")
-					all_upward_requests[peer_update.New] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-					all_downward_requests[peer_update.New] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-					all_command_requests[peer_update.New] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-				}
+
+				request_holder.Attempt_init_id(peer_update.New)
 
 				if peer_update.New != local_id {
-					update_own_state_varialbe_and_send_to_network(all_elevator_states[local_id])
+					update_own_state_variable_and_send_to_network(all_elevator_states[local_id])
 				}
 
-				// new peer, send all stored requests on network
-				for _, requests_list_by_id := range []map[string][]message_structs.Request{all_upward_requests, all_downward_requests, all_command_requests} {
-					for _, request_list_by_floor := range requests_list_by_id {
-						for _, request := range request_list_by_floor {
-							if request != zero_request {
-								distribute_request(request)
-							}
-						}
+				for _, request := range request_holder.Get_all_requests() {
+					if request != zero_request {
+						distribute_request(request)
 					}
 				}
 			}
@@ -95,21 +80,17 @@ func Distribute_requests(
 					fmt.Println("Lost elevator: ", lost_elevator_id)
 					if lost_elevator_id != local_id {
 						fmt.Println("Lost non-local elevator: ", lost_elevator_id)
-						//delete elevator and Inherit requests
 						delete(all_elevator_states, lost_elevator_id)
-						for _, requests_list_by_id := range []map[string][]message_structs.Request{all_upward_requests, all_downward_requests, all_command_requests} {
-							for _, request := range requests_list_by_id[lost_elevator_id] {
-								if request != zero_request {
-									if request.Request_type != hardware_interface.BUTTON_TYPE_COMMAND {
-										delete_single_request(request)
-										request.Responsible_elevator = local_id
-									}
-									if request == zero_request { // TEMP TEST
-										fmt.Println("Peer lost, trying to register zero request. ")
-									}
-									distribute_request(request)
-									register_request(request)
+
+						for _, request := range request_holder.Get_all_requests_for_id(lost_elevator_id) {
+							if request != zero_request {
+								if request.Request_type != hardware_interface.BUTTON_TYPE_COMMAND {
+									request_holder.Delete_single_request(request) //in holder
+									request_watchdog.Timer_stop(request)
+									request.Responsible_elevator = local_id
 								}
+								distribute_request(request)
+								register_request(request)
 							}
 						}
 					}
@@ -117,7 +98,7 @@ func Distribute_requests(
 			}
 
 		case local_elevator_state := <-local_elevator_state_changes_chan:
-			update_own_state_varialbe_and_send_to_network(local_elevator_state)
+			update_own_state_variable_and_send_to_network(local_elevator_state)
 
 		case button_request := <-button_request_chan:
 			button_request.Responsible_elevator = decide_responsible_elevator(button_request)
@@ -180,31 +161,17 @@ func register_request(request message_structs.Request) {
 	if request.Responsible_elevator == "" {
 		fmt.Println("Trying to register blank id! ERROR")
 	}
-	if _, ok := all_command_requests[request.Responsible_elevator]; !ok {
-		fmt.Println("saved register!")
-		all_upward_requests[request.Responsible_elevator] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_downward_requests[request.Responsible_elevator] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_command_requests[request.Responsible_elevator] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-	}
 	request_watchdog.Timer_start(request)
-	switch request.Request_type {
-	case hardware_interface.BUTTON_TYPE_CALL_UP:
-		if all_upward_requests[request.Responsible_elevator][request.Floor] == zero_request {
-			all_upward_requests[request.Responsible_elevator][request.Floor] = request
-			print_request_list()
-		}
-	case hardware_interface.BUTTON_TYPE_CALL_DOWN:
-		if all_downward_requests[request.Responsible_elevator][request.Floor] == zero_request {
-			all_downward_requests[request.Responsible_elevator][request.Floor] = request
-			print_request_list()
-		}
-	case hardware_interface.BUTTON_TYPE_COMMAND:
-		if all_command_requests[request.Responsible_elevator][request.Floor] == zero_request {
-			all_command_requests[request.Responsible_elevator][request.Floor] = request
-			print_request_list()
-		}
-	}
+	request_holder.Hold_request(request)
 	set_request_lights(request, 1)
+	request_holder.Print_requests(all_elevator_states)
+}
+
+func register_finished_request(request message_structs.Request) {
+	set_request_lights(request, 0)
+	request_holder.Delete_floor_requests_not_uncompleted_command(request)
+	request_watchdog.Timer_stop(request)
+	request_holder.Print_requests(all_elevator_states)
 }
 
 func distribute_request(request message_structs.Request) {
@@ -220,7 +187,7 @@ func distribute_request(request message_structs.Request) {
 	}
 }
 
-func update_own_state_varialbe_and_send_to_network(elevator_state message_structs.Elevator_state) {
+func update_own_state_variable_and_send_to_network(elevator_state message_structs.Elevator_state) {
 	elevator_state.Elevator_id = local_id
 	all_elevator_states[local_id] = elevator_state
 
@@ -228,33 +195,6 @@ func update_own_state_varialbe_and_send_to_network(elevator_state message_struct
 		local_elevator_state_changes_tx_chan <- elevator_state
 		<-time.After(time.Millisecond * network_transmit_wait_time)
 	}
-}
-
-func register_finished_request(request message_structs.Request) {
-	set_request_lights(request, 0)
-	delete_request_except_non_completed_command(request)
-	print_request_list()
-}
-
-func delete_request_except_non_completed_command(request message_structs.Request) {
-	all_upward_requests[request.Responsible_elevator][request.Floor] = zero_request
-	all_downward_requests[request.Responsible_elevator][request.Floor] = zero_request
-	if request.Is_completed {
-		all_command_requests[request.Responsible_elevator][request.Floor] = zero_request
-	}
-	request_watchdog.Timer_stop(request)
-}
-
-func delete_single_request(request message_structs.Request) {
-	switch request.Request_type {
-	case hardware_interface.BUTTON_TYPE_CALL_DOWN:
-		all_downward_requests[request.Responsible_elevator][request.Floor] = zero_request
-	case hardware_interface.BUTTON_TYPE_CALL_UP:
-		all_upward_requests[request.Responsible_elevator][request.Floor] = zero_request
-	case hardware_interface.BUTTON_TYPE_COMMAND:
-		all_command_requests[request.Responsible_elevator][request.Floor] = zero_request
-	}
-	request_watchdog.Timer_stop(request)
 }
 
 func set_request_lights(request message_structs.Request, value int) {
@@ -312,14 +252,6 @@ func decide_responsible_elevator(request message_structs.Request) string {
 func estimate_time_for_elevator_to_complete_request(elevator_state message_structs.Elevator_state, request message_structs.Request) int {
 	elevator_id := elevator_state.Elevator_id
 
-	if _, ok := all_command_requests[elevator_id]; !ok {
-		fmt.Println("Saved estimate!")
-		all_upward_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_downward_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-		all_command_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
-	}
-
-	request_list_by_motor_direction := map[int]map[string][]message_structs.Request{hardware_interface.MOTOR_DIRECTION_DOWN: all_downward_requests, hardware_interface.MOTOR_DIRECTION_UP: all_upward_requests}
 	endfloor_in_motor_direction := map[int]int{hardware_interface.MOTOR_DIRECTION_DOWN: -1, hardware_interface.MOTOR_DIRECTION_UP: hardware_interface.N_FLOORS}
 
 	total_time := 0
@@ -327,7 +259,10 @@ func estimate_time_for_elevator_to_complete_request(elevator_state message_struc
 	last_stop_floor := starting_floor
 	starting_motor_direction := elevator_state.Last_non_stop_motor_direction
 
+	all_command_requests := request_holder.Get_requests_of_type(hardware_interface.BUTTON_TYPE_COMMAND)
+
 	for _, motor_direction := range []int{starting_motor_direction, -starting_motor_direction, starting_motor_direction} { // Using that motor directions are +/-1. See hardware_interface for definitions
+		requests_for_direction := request_holder.Get_requests_for_motor_direction(motor_direction)
 		for floor := starting_floor; floor != endfloor_in_motor_direction[motor_direction]; floor += motor_direction {
 			if floor == request.Floor {
 				total_time += abs(last_stop_floor-floor) * request_executor.ESTIMATED_TIME_BETWEEN_FLOORS
@@ -340,7 +275,7 @@ func estimate_time_for_elevator_to_complete_request(elevator_state message_struc
 				}
 			}
 
-			if request_list_by_motor_direction[motor_direction][elevator_id][floor] != zero_request || all_command_requests[elevator_id][floor] != zero_request {
+			if requests_for_direction[elevator_id][floor] != zero_request || all_command_requests[elevator_id][floor] != zero_request {
 				total_time += abs(last_stop_floor-floor) * request_executor.ESTIMATED_TIME_BETWEEN_FLOORS
 				if request.Floor == floor {
 					return total_time
@@ -361,51 +296,4 @@ func abs(num int) int {
 		return -num
 	}
 	return num
-}
-
-func print_request_list() {
-	var sorted_ids []string
-	for id := range all_command_requests {
-		sorted_ids = append(sorted_ids, id)
-	}
-	sort.Strings(sorted_ids)
-
-	fmt.Print("\n\n\n\n")
-	for _, responsible_id := range sorted_ids {
-		fmt.Print("\n")
-		fmt.Println("Responsible: ", responsible_id)
-		fmt.Println("--------------------------------------")
-		fmt.Println("\tFLOOR\tUP\tDOWN\tCOMMAND")
-		for floor := hardware_interface.N_FLOORS - 1; floor >= 0; floor-- {
-			fmt.Print("\t", floor, "\t")
-			if all_upward_requests[responsible_id][floor] != zero_request {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-			fmt.Print("\t")
-			if all_downward_requests[responsible_id][floor] != zero_request {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-			fmt.Print("\t")
-			if all_command_requests[responsible_id][floor] != zero_request {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-			fmt.Print("\t")
-			if all_elevator_states[responsible_id].Last_visited_floor == floor {
-				fmt.Print("#")
-			} else {
-				fmt.Print(" ")
-			}
-			fmt.Print("\n")
-		}
-		fmt.Println("--------------------------------------")
-		fmt.Print("\n")
-	}
-	fmt.Print("\n\n\n\n")
-
 }
---
+++ project/request_distributor/request_holder/request_holder.go
@@ -0,0 +1,165 @@
+package request_holder
+
+import (
+	"../../hardware_interface"
+	"../../message_structs"
+	"fmt"
+	"sort"
+)
+
+var all_upward_requests = make(map[string][]message_structs.Request)
+var all_downward_requests = make(map[string][]message_structs.Request)
+var all_command_requests = make(map[string][]message_structs.Request)
+
+var zero_request message_structs.Request = message_structs.Request{}
+
+func Attempt_init_id(elevator_id string) {
+	attempt_init_request_lists_for_id(elevator_id)
+}
+
+func attempt_init_request_lists_for_id(elevator_id string) {
+	if _, ok := all_command_requests[elevator_id]; !ok {
+		all_upward_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
+		all_downward_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
+		all_command_requests[elevator_id] = make([]message_structs.Request, hardware_interface.N_FLOORS)
+	}
+}
+
+func Hold_request(request message_structs.Request) {
+	if request.Responsible_elevator == "" {
+		return
+	}
+	attempt_init_request_lists_for_id(request.Responsible_elevator)
+
+	switch request.Request_type {
+	case hardware_interface.BUTTON_TYPE_CALL_UP:
+		if all_upward_requests[request.Responsible_elevator][request.Floor] == zero_request { //in holder
+			all_upward_requests[request.Responsible_elevator][request.Floor] = request
+		}
+	case hardware_interface.BUTTON_TYPE_CALL_DOWN:
+		if all_downward_requests[request.Responsible_elevator][request.Floor] == zero_request { //
+			all_downward_requests[request.Responsible_elevator][request.Floor] = request
+		}
+	case hardware_interface.BUTTON_TYPE_COMMAND:
+		if all_command_requests[request.Responsible_elevator][request.Floor] == zero_request { //
+			all_command_requests[request.Responsible_elevator][request.Floor] = request
+		}
+	}
+}
+
+func Delete_single_request(request message_structs.Request) {
+	switch request.Request_type {
+	case hardware_interface.BUTTON_TYPE_CALL_DOWN:
+		all_downward_requests[request.Responsible_elevator][request.Floor] = zero_request
+	case hardware_interface.BUTTON_TYPE_CALL_UP:
+		all_upward_requests[request.Responsible_elevator][request.Floor] = zero_request
+	case hardware_interface.BUTTON_TYPE_COMMAND:
+		all_command_requests[request.Responsible_elevator][request.Floor] = zero_request
+	}
+}
+
+func Delete_floor_requests_not_uncompleted_command(request message_structs.Request) {
+	all_upward_requests[request.Responsible_elevator][request.Floor] = zero_request
+	all_downward_requests[request.Responsible_elevator][request.Floor] = zero_request
+	if request.Is_completed {
+		all_command_requests[request.Responsible_elevator][request.Floor] = zero_request
+	}
+}
+
+func Get_all_requests() []message_structs.Request {
+	all_requests := make([]message_structs.Request, 0)
+
+	for _, requests_list_by_id := range []map[string][]message_structs.Request{all_upward_requests, all_downward_requests, all_command_requests} {
+		for _, requests_by_floor := range requests_list_by_id {
+			for _, request := range requests_by_floor {
+				all_requests = append(all_requests, request)
+			}
+		}
+	}
+	return all_requests
+}
+
+func Get_all_requests_for_id(elevator_id string) []message_structs.Request {
+	attempt_init_request_lists_for_id(elevator_id)
+
+	all_requests_for_id := make([]message_structs.Request, 0)
+
+	for _, requests_list_for_id := range []map[string][]message_structs.Request{all_upward_requests, all_downward_requests, all_command_requests} {
+		for _, request := range requests_list_for_id[elevator_id] {
+			if request != zero_request {
+				all_requests_for_id = append(all_requests_for_id, request)
+			}
+		}
+	}
+	return all_requests_for_id
+}
+
+func Get_requests_for_motor_direction(motor_direction int) map[string][]message_structs.Request {
+	switch motor_direction {
+	case hardware_interface.MOTOR_DIRECTION_UP:
+		return all_upward_requests
+	case hardware_interface.MOTOR_DIRECTION_DOWN:
+		return all_downward_requests
+	case hardware_interface.MOTOR_DIRECTION_STOP:
+		return all_downward_requests
+	}
+	return make(map[string][]message_structs.Request)
+}
+
+func Get_requests_of_type(request_type int) map[string][]message_structs.Request {
+	switch request_type {
+	case hardware_interface.BUTTON_TYPE_CALL_UP:
+		return all_upward_requests
+	case hardware_interface.BUTTON_TYPE_CALL_DOWN:
+		return all_downward_requests
+	case hardware_interface.BUTTON_TYPE_COMMAND:
+		return all_downward_requests
+	}
+	return make(map[string][]message_structs.Request)
+}
+
+func Print_requests(all_elevator_states map[string]message_structs.Elevator_state) {
+	var sorted_ids []string
+	for id := range all_command_requests {
+		sorted_ids = append(sorted_ids, id)
+	}
+	sort.Strings(sorted_ids)
+
+	fmt.Print("\n\n\n\n")
+	for _, responsible_id := range sorted_ids {
+		fmt.Print("\n")
+		fmt.Println("Responsible: ", responsible_id)
+		fmt.Println("--------------------------------------")
+		fmt.Println("\tFLOOR\tUP\tDOWN\tCOMMAND")
+		for floor := hardware_interface.N_FLOORS - 1; floor >= 0; floor-- {
+			fmt.Print("\t", floor, "\t")
+			if all_upward_requests[responsible_id][floor] != zero_request {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Print("\t")
+			if all_downward_requests[responsible_id][floor] != zero_request {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Print("\t")
+			if all_command_requests[responsible_id][floor] != zero_request {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Print("\t")
+			if all_elevator_states[responsible_id].Last_visited_floor == floor {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Print("\n")
+		}
+		fmt.Println("--------------------------------------")
+		fmt.Print("\n")
+	}
+	fmt.Print("\n\n\n\n")
+}
